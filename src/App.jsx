b4403345e5462317b1bfcb77a89f402ee642fcc6
shubhamyadav0533-commits <shubhamyadav0533@gmail.com>
2025-11-08 00:52:04 +0530

    This is when i just updated SEO and add privacy police page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes,Route } from "react-router-dom"
-import { HelmetProvider } from "react-helmet-async"
 import './App.css'
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home/Home"
@@ -24,29 +23,27 @@ function App() {
 
   return (
     <CartProvider>
-      <HelmetProvider>
-        <Router>
-          <ScrollToTop />
-          <Navbar/>
-          <SmoothScroll/>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/contact" element={<Contacts/>} />
-            <Route path="/blogs" element={<Blogs/>} />
-            <Route path="/blog/:id" element={<BlogDetail/>} />
-            <Route path="/services" element={<Services/>} />
-            <Route path="/cart" element={<Cart/>} />
-            <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="/checkout" element={<Checkout/>} />
-            <Route path="/products" element={<Products/>} />
-            <Route path="/articles/:id" element={<BlogDetail/>} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="*" element={<Error404/>} />
-          </Routes>
-          <Footer/>
-        </Router>
-      </HelmetProvider>
+      <Router>
+        <ScrollToTop />
+        <Navbar/>
+        <SmoothScroll/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contacts/>} />
+          <Route path="/blogs" element={<Blogs/>} />
+          <Route path="/blog/:id" element={<BlogDetail/>} />
+          <Route path="/services" element={<Services/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/checkout" element={<Checkout/>} />
+          <Route path="/products" element={<Products/>} />
+          <Route path="/articles/:id" element={<BlogDetail/>} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="*" element={<Error404/>} />
+        </Routes>
+        <Footer/>
+      </Router>
     </CartProvider>
   )
 }
